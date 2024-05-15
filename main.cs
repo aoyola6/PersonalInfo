048ff17386c18f2937c86c9b502fa5255e68065a
aoyola6 <116612522+aoyola6@users.noreply.github.com>
2024-05-15 22:22:25 +0000

    Unit Test 4 Commit

--- main.cs
+++ main.cs
@@ -2,16 +2,16 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-   string name =  "Chris Johnson";
+   string name =  "Robert Hall";
     Console.WriteLine("Name: " + name); 
     
-    DateOnly dateofBirth = new DateOnly(1987, 12, 1);
+    DateOnly dateofBirth = new DateOnly(1949, 2, 27);
     Console.WriteLine("Date of Birth: " + dateofBirth);
     
-    string workNumber = "000-222-0101";
+    string workNumber = "000-444-0101";
     Console.WriteLine("Work Phone: " + workNumber);
     
-    string cellNumber = "000-222-0189";
+    string cellNumber = "000-444-0189";
     Console.WriteLine("Cell Phone: " + cellNumber);
   }
 } 
